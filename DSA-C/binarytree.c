6b10858aa81ac60dae413f0b22c5624cc52ed49c
Nonamechick <otajonovxasan350@gmail.com>
2025-04-16 22:48:54 +0500

    tree

--- DSA-C/binarytree.c
+++ DSA-C/binarytree.c
@@ -27,3 +27,27 @@ void preorder(struct Node* root) {
         preorder(root->right);
     }
 }
+void postorder(struct Node* root) {
+    if(root!=NULL) {
+        postorder(root->left);
+        postorder(root->right);
+        printf("%d ", root->data);
+    }
+}
+int main() {
+    struct Node* root = createNode(1);
+    root->left = createNode(2);
+    root->right = createNode(3);
+    root->left->left = createNode(4);
+    root->left->right = createNode(5);
+    printf("Inorder traversal: ");
+    inorder(root);
+    printf("\n");
+    printf("Preorder traversal: ");
+    preorder(root);
+    printf("\n");
+    printf("Postorder traversal: ");
+    postorder(root);
+    printf("\n");
+    return 0;
+}
